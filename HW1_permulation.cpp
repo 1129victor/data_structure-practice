56624dcf65dd19cc247b73d75f37ab56bfe508b7
1129victor <bmw61102@gmail.com>
2017-09-28 13:24:10 +0800

    234

---
+++ HW1_permulation.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 100 
+#define SWAP(x, y, t) ((t) = (x), (x) = (y), (y) = (t))
+
+void perm(char *, int, int);
+
+int main (void)
+{
+	char openfile[MAX_SIZE];
+	char list[MAX_SIZE];
+	int n;
+	FILE *cfptr;
+	
+	printf("輸入讀取檔案:\n");
+	while(scanf("%s", openfile) != EOF){
+		
+		if((cfptr = fopen(openfile, "r")) == NULL){
+			puts("File could not be opened");
+		}else{
+			fscanf(cfptr, "%s", list);
+			
+			n = strlen(list);
+			
+			perm(list, 0, n - 1);
+		}
+		
+		fclose(cfptr);
+		printf("\n");
+		printf("輸入讀取檔案:\n");
+	}	
+	
+	return 0;
+} 
+
+void perm(char *list, int i, int n)
+{
+	int j, temp;
+	
+	if(i == n){
+		for(j = 0; j <= n; j++){
+			printf("%c", list[j]);
+		}
+		printf(" ");
+	}else{
+		for(j = i; j <= n; j++){
+			SWAP(list[i], list[j], temp);	
+			perm(list, i + 1, n);
+			SWAP(list[i], list[j], temp);
+		}
+	}
+}
